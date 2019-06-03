bcd92345375cf6093b2584e6f18d4f24670da610
changlei <2383788860@qq.com>
2019-06-03 21:22:01 +0800

    完成在线拍轮播框

--- Auction/src/app/app.module.ts
+++ Auction/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
