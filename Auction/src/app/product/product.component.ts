7f2f04824998dca1aa42c24640a5c18af6339ac7
changlei <2383788860@qq.com>
2019-06-19 16:11:18 +0800

    完成星级评价

--- Auction/src/app/product/product.component.ts
+++ Auction/src/app/product/product.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  private products: Array<Product>;
+  private imgUrl = 'http://placehold.it/321x150';
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      new Product(1, '第一个商品', 1.99, 3.5, '这是第一个商品', ['电子产品', '硬件设备']),
+      new Product(1, '第二个商品', 2.99, 4.5, '这是第二个商品', ['电子产品', '硬件设备']),
+      new Product(1, '第三个商品', 3.99, 4.5, '这是第三个商品', ['电子产品', '硬件设备']),
+      new Product(1, '第四个商品', 4.99, 2.5, '这是第四个商品', ['电子产品', '硬件设备']),
+      new Product(1, '第五个商品', 5.99, 1.5, '这是第五个商品', ['电子产品', '硬件设备'])
+    ];
   }
 
+}
+export class Product {
+  constructor(
+    public id: number,  // 商品ID
+    public title: string, // 商品名称
+    public price: number, // 商品价格
+    public rating: number,  // 商品星级
+    public desc: string,  // 商品描述
+    public categories: Array<string>  // 商品类别
+  ) {
+
+  }
 }
--- Auction/src/app/stars/stars.component.ts
+++ Auction/src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
-
+  @Input()
+  private rating = 0;
+  private stars: Array<boolean>; // 星星图标是否为实心 true为空心 false为实心
   constructor() { }
 
   ngOnInit() {
+    this.stars = [];
+    for (let index = 1; index <= 5; index++) { // 根据星级确定星星的显示
+      if (index < this.rating) {
+        this.stars.push(false);
+      } else {
+        this.stars.push(true);
+      }
+    }
   }
 
 }
